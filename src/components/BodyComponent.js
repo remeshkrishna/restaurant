b5061ebfdc86f63e7980e55e375441e765155e68
unknown <remeshkrishnaur@gmail.com>
2025-03-05 20:35:41 +0530

    added feature for filter restaurants by searching name

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,19 +1,53 @@
-import RestaurantCard from "./RestaurantCard";
 import restaurants from "../utils/mockData";
-import { useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+//import restaurants from "../utils/mockData";
+import { useState , useEffect} from "react";
+import Shimmer from "./Shimmer";
 
-const BodyComponent = ()=>{
-    let [resData,setResData] = useState(restaurants);
+const BodyComponent =  ()=>{
+    let [resData,setResData] = useState([]);
+    let [filteredResData,setFilteredResData] = useState([]);
+    // let [searchData,setSearchData] = useState("");
+    const fetchData = async ()=>{
+        const resp = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.01020&lng=76.97010&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const respJson = await resp.json();
+        //optional chaining if there is no data, instaed of error it will return undefined
+        restaurants = respJson?.data?.cards?.filter((item)=>item?.card?.card?.id=="top_brands_for_you")[0]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        console.log(restaurants)
+        setResData(restaurants);
+        setFilteredResData(restaurants);
+    }
+    useEffect(()=>{
+        fetchData();
+    },[])
+
+    let topRatedFilter = ()=>{
+        let filtered = resData.filter((item)=>item.info.avgRating>4.5);
+        setFilteredResData(filtered);
+        }
+    
+    searchHandler = ()=>{
+        let searchText = document.getElementById("search-box").value;
+        let filtered = resData.filter((item)=>item.info.name.toLowerCase().includes(searchText.toLowerCase()))
+        setFilteredResData(filtered)
+
+    }
+
+    //conditional rendering
+    if(resData.length==0){
+        return <Shimmer/>
+    }
     return (
     <div className="body">
         <div className="filter-container">
-            <button className="filter-btn" onClick={()=>{
-                filteredResData = resData.filter((item)=>item.info.avgRating>4);
-                setResData(filteredResData);
-                }}>Top Rated Restaurants</button>
+            <div className="search-container">
+                <input type="text" id="search-box"/>
+                <button onClick={searchHandler}>Search</button>
+            </div>
+            <button className="filter-btn" onClick={topRatedFilter}>Top Rated Restaurants</button>
         </div>
         <div className="restaurant-card-container">
-            {resData.map((rest)=> <RestaurantCard key={rest.info.id} props={rest.info}/> )}
+            {filteredResData.map((rest)=> <RestaurantCard key={rest.info.id} props={rest.info}/> )}
         </div>
     </div>
 );}
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import {LOGO_URL} from "../utils/constants"
 
 const HeaderComponent = ()=>{
+    let [loginState,setLoginState] =useState(false)
     return (<div className="header">
         <div className="logo-container">
             <img className="logo" src={LOGO_URL}/>
@@ -11,6 +13,9 @@ const HeaderComponent = ()=>{
                 <li>About</li>
                 <li>Contact</li>
                 <li>Cart</li>
+                <button onClick={()=>{
+                    setLoginState(!loginState)
+                }} style={loginState?{backgroundColor: "green"}:{backgroundColor: "rgb(201, 198, 198)"}}>{loginState?"Logout":"Login"}</button>
             </ul>
         </div>
     </div>)
